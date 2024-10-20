16b8e70dbe2ca68c424e9647117495e2fd0f204a
yotaro <blog.ysk.everyday@gmail.com>
2024-10-20 11:06:32 +0900

    PostModelの作成
    RepositoryもModelに合わせて微修正

--- practice/lib/post/post_repository.dart
+++ practice/lib/post/post_repository.dart
@@ -1,13 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:practice/post/post_model.dart';
 
 class PostRepository {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<void> addPost(
+  Future<void> addPost({
     required Post post,
-  ) async {
-    final Map<String, dynamic> postMap = post.tojson();
+  }) async {
+    final Map<String, dynamic> postMap = post.toJson();
     await _firestore.collection('posts').doc().set(postMap);
   }
 }
